84d06ad8cb396c2d70ab005cf93b27d3a6d0cd2c
vinamraj <jvinamra776@gmail.com>
2019-01-19 19:36:02 +0530

    Initial Project Add

---
+++ src/main/java/com/vinamra/vjpetclinic/VjPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.vinamra.vjpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VjPetClinicApplication.class, args);
+    }
+
+}
+
