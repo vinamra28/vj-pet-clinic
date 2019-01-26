5b84d6c7e1bf2de98d8b55a53b3666da9e5f0bfa
vinamraj <jvinamra776@gmail.com>
2019-01-26 09:16:05 +0530

    added properties. closes #28

--- pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Owner.java
@@ -4,13 +4,40 @@ import java.util.Set;
 
 public class Owner extends Person
 {
-    private Set<Pet> petSet;
+    private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
 
-    public Set<Pet> getPetSet() {
-        return petSet;
+    public Set<Pet> getPets() {
+        return pets;
     }
 
-    public void setPetSet(Set<Pet> petSet) {
-        this.petSet = petSet;
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
