0dcc2fe4ac4de0e3b9e7c092e39ae0fe7cc4023f
vinamraj <jvinamra776@gmail.com>
2019-01-26 09:10:21 +0530

    added specialities, added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.vinamra.vjpetclinic.model;
+
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
