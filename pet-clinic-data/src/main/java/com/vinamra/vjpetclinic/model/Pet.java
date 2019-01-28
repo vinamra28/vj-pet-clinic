70a3593a98f360608ec6cc21e3d33aa06dd5ffae
vinamraj <jvinamra776@gmail.com>
2019-01-28 18:49:18 +0530

    Added Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.vinamra.vjpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,8 +19,12 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
