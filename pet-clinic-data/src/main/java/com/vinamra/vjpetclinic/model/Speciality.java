7a0cd1878b8543b9733bb71f660e2a0bef1c5c36
vinamraj <jvinamra776@gmail.com>
2019-01-28 18:39:23 +0530

    updated vets with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.vinamra.vjpetclinic.model;
 
-public class Speciality extends BaseEntity
-{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
+public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/vinamra/vjpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package com.vinamra.vjpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    //if it's lazy then it does not load anything until it's asked
+    @ManyToMany(fetch = FetchType.EAGER)//this means that JPA will try to load everything at once
+    @JoinTable(name = "vet_specialities",joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
